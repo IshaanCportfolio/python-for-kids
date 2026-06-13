b3670a026a97575361fe16e6547ee99174d337be
IshaanChandan <ishaanchandan@gmail.com>
2026-06-13 13:06:02 -0400

    Chapter 3
    Chapter 3: creatin strings and multiplying strings.

---
+++ Chapter-03-strings-lists-tuples-dictionnaries/03-creating-strings.py
@@ -0,0 +1,19 @@
+fred='''how do dinosaurs pay their bills?
+with tyrannosaurus checks!'''
+print(fred)
+silly_string='''He said, "Aren't can't shouldn't wouldn't."'''
+print(silly_string)
+single_quote_str='He said, "Aren\'t can\'t shouldn\'t wouldn\'t."'
+double_quote_str="He said, \"Aren\'t can\'t shouldn\'t wouldn\'t.\""
+print(single_quote_str)
+print(double_quote_str)
+my_score=10
+message=f'I scored {my_score} points.'
+print(message)
+print(f'I scored {my_score} points.')
+senators=9
+maple_leafs=3
+print(f'The senators destroyed the maple leafs {senators}, to {maple_leafs}!')
+print(f'The senators destroyed the maple leafs {senators} to {maple_leafs}!')
+print(f'The product of 18*4 equals {18*4}.')
+print(10*'a')
