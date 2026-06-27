0668647df44f1d01b5ecd9540afb5300dd0089a3
IshaanChandan <ishaanchandan@gmail.com>
2026-06-27 19:27:52 -0400

    Create 03-making-lists-adding-and-removing-items-arithmetic.py
    To create a list you need to use square brackets and you can add lists to other lists and multiply lists by a value (number), but lists can not be subtracted or divided.

---
+++ Chapter-03-strings-lists-tuples-dictionnaries/03-making-lists-adding-and-removing-items-arithmetic.py
@@ -0,0 +1,27 @@
+wizard_list='spider legs, toe of frog, bat wing, slug butter and snake dandruff.'
+print(wizard_list)
+wizard_list=['spider legs', 'toe of frog', 'bat wing', 'slug butter', 'snake dandruff']
+print(wizard_list)
+print(wizard_list[2])
+wizard_list[2]='snail tongue'
+print(wizard_list)
+print(wizard_list[2:5])
+wizard_list.append('bear bump')
+print(wizard_list)
+wizard_list.append('mandrake')
+wizard_list.append('hemlock')
+wizard_list.append('swamp gas')
+print(wizard_list)
+del wizard_list[4]
+print(wizard_list)
+del wizard_list[7]
+del wizard_list[6]
+del wizard_list[5]
+print(wizard_list)
+list1=[1, 2, 3, 4]
+list2=['I', 'tripped', 'over', 'and', 'hit', 'the', 'floor']
+print(list1+list2)
+list3=list1+list2
+print(list3)
+list1=[1,2]
+print(list1*5)
